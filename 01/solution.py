fbb43f5d2d7af1e869ac4ecd11d0666f7f633fb2
Keith Rozario <keith@keithrozario.com>
2024-12-08 14:34:51 +0800

    added day 4

--- 01/solution.py
+++ 01/solution.py
@@ -5,7 +5,7 @@ list_1, list_2 = [], []
 with open('./01/input.txt', 'r') as input:
     lines = input.readlines()
     for line in lines:
-        num_1, num_2 = line.replace("\n","").split("   ")
+        num_1, num_2 = line.strip().split("   ")
         list_1.append(int(num_1))
         list_2.append(int(num_2))
 
@@ -15,10 +15,10 @@ list_2.sort()
 
 # Get diff into a single list
 total_diff = 0
-for i in range(len(list_1)):
-    total_diff += abs(list_2[i] - list_1[i])
+for i,num in enumerate(list_1):
+    total_diff += abs(list_2[i] - num)
     
-print(total_diff)
+print(f"The total diff is: {total_diff}")
 
 # now get similarity score
 total_similarity = 0
@@ -26,4 +26,4 @@ for num in list_1:
     num_occurence = list_2.count(num)
     total_similarity += num * num_occurence
 
-print(total_similarity)
+print(f"The total similarity is: {total_similarity}")
--- 02/solution.py
+++ 02/solution.py
@@ -12,46 +12,52 @@ def check_descending(report):
     return True
 
 def check_diff(report):
+    """
+    Returns true if all elements in report are within 3 of each other
+    """
     for i in range(1, len(report)):
         if abs(report[i] - report[i-1]) > 3:
             return False
     return True
 
-def check(report):
+def check_safety(report: list)->bool:
+    """
+    Returns true if report is safe, false otherwise
+    """
     if check_ascending(report) or check_descending(report):
         if check_diff(report):
             return True    
     return False
 
-# get input
-with open("./02/input.txt","r") as input:
-    lines = input.readlines() 
-    reports = [line.strip().split(" ") for line in lines]
-
-# count good reports
-good_report_count = 0
-bad_reports = []
-for report in reports:
-    report_num = [int(x) for x in report]
-    if check(report_num):
-        good_report_count += 1
-    else:
-        bad_reports.append(report_num)
-
-print(good_report_count)
-
-# Problem Dampener
-dampened_report_count = 0
-for report in bad_reports:
-    for i in range(len(report)):
+def check_safety_with_dampener(report: list)->bool:
+    """
+    Checks for report will be safe if one value is removed
+    """
+    for i, num in enumerate(report):
         # remove one element at a time
         report_copy = report.copy()
         report_copy.pop(i)
-        
         # check if report is now safe
-        if check(report_copy):
-            dampened_report_count += 1
-            # no longer have to check this report it's already safe
-            break
+        if check_safety(report_copy):
+            return True
+    return False
 
-print(good_report_count + dampened_report_count)
+# get input
+with open("./02/input.txt","r") as input:
+    lines = input.readlines() 
+    report_strings = [line.strip().split(" ") for line in lines]
+    # convert all elements from str to int
+    reports = [[int(x) for x in report] for report in report_strings]
+
+
+safe_reports = [report for report in reports if check_safety(report)]
+print(f"Number of safe reports: {len(safe_reports)}")
+
+# Problem Dampener
+unsafe_reports = [report for report in reports if not check_safety(report)]
+safe_reports_after_dampener = [
+    report for report in unsafe_reports if check_safety_with_dampener(report)
+]
+
+print(f"Number of safe reports with damperner: {len(safe_reports_after_dampener)}")
+print(f"Total safe reports with dampening: {len(safe_reports)+len(safe_reports_after_dampener)}")
